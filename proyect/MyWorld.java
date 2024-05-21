1870256cdd6f8b893e0251e22008526919cce25e
Juan Ordonez <ord24979@uvg.edu.gt>
2024-05-20 19:53:11 -0600

    Cambio de nombre en clase Enemies a Zombie, agregue programa en clase MyWorld para que se generen 4 zombies aleatorios

--- proyect/MyWorld.java
+++ proyect/MyWorld.java
@@ -30,5 +30,38 @@ public class MyWorld extends World
         Hero hero = new Hero();
         addObject(hero,112,136);
         hero.setLocation(292,322);
+        
+        
+        spawnZombie();
+    }
+    
+    private void spawnZombie()
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            Zombie zombie = new Zombie();
+            int side = Greenfoot.getRandomNumber(4);
+            int x, y;
+            if (side == 0) {
+                x = Greenfoot.getRandomNumber(getWidth());
+                y = 0;
+            }
+            else if (side == 1)
+            {
+                x = Greenfoot.getRandomNumber(getWidth());
+                y = getHeight() - 1;
+            }
+            else if (side == 2)
+            {
+                x = 0;
+                y = Greenfoot.getRandomNumber(getHeight());
+            }
+            else
+            {
+                x = getWidth() -1;
+                y = Greenfoot.getRandomNumber(getHeight());
+            }
+            addObject(zombie, x, y);
+        }
     }
 }
