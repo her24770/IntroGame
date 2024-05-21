44cffa4f913d384132c5dce2a27a2b6ed691e133
ESJ0 <usuarioj0s3p1@gmail.com>
2024-05-20 20:58:35 -0600

    AYIYIYIYIY

--- proyect/Obstacles.java
+++ proyect/Obstacles.java
@@ -2,7 +2,16 @@ import greenfoot.*;
 
 public class Obstacles extends Actor
 {
+    public Obstacles()
+    {
+        // Define el tamaño y apariencia del obstáculo
+        GreenfootImage image = new GreenfootImage(50, 50); // Tamaño del bloque (50x50 píxeles)
+        image.setColor(Color.GRAY); // Color del bloque
+        image.fill(); // Rellena el bloque con el color especificado
+        setImage(image); // Establece la imagen del obstáculo
+    }
     public void act()
+    
     {
         // Add your action code here.
     }
